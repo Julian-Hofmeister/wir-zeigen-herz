76f2ad0491bbe2bddb1692385f8ab8a6b5bb8b34
Julian Hofmeister <hjulian@cubefour.de>
2023-06-18 06:37:53 +0200

    Added everything to git

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,27 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.wirzeigenherz',
-  appName: 'Wir zeigen Herz',
+  appId: 'eu.wirzeigenherz.app',
+  appName: 'wir-zeigen-herz',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      // backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      // androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      // androidSpinnerStyle: "large",
+      // iosSpinnerStyle: "small",
+      // spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      // layoutName: "launch_screen",
+      // useDialog: true,
+    },
+  },
 };
 
 export default config;
