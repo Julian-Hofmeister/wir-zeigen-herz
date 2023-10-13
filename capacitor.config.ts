2e368859d391f9161212d5953b5db56a908365b9
julian hofmeister <ju.hofmeister@web.de>
2023-10-13 15:27:48 +0200

    Merge remote-tracking branch 'origin/master'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.wirzeigenherz.app',
+  appId: 'eu.wirzeigenherz.app',
   appName: 'Wir zeigen Herz 2.0',
   webDir: 'www',
   bundledWebRuntime: false
